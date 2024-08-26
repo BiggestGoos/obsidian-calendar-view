fb04895c3e95b6c810ba4b696931d45612f493d4
Gustav Fagerlind <gustav@fagerlind.net>
2024-08-26 18:51:02 +0200

    Started from scratch to ensure that we don't keep reduntant example stuff, started working on the calendar view and how it should open/work.

--- main.ts
+++ main.ts
@@ -1,141 +1,85 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin, ItemView, WorkspaceLeaf, TFile, MarkdownView, FileManager } from 'obsidian';
 
 export default class HelloWorldPlugin extends Plugin {
-	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
+		
+        console.log('load')
 
-		this.addRibbonIcon('dice', 'Greet', () => {
-			new Notice('Hello, world! test23 ' + this.app.vault.getName());
-			this.app.vault.create(this.app.workspace.getLeaf().getDisplayText() + '1.md', 'test text - hello world')
+        this.registerView(
+			VIEW_CALENDAR_VIEW,
+			(leaf) => new Calendar_View(leaf)
+		);
+
+        this.addRibbonIcon("calendar-check-2", "Calendar View", () => {
+			// this.activateView();
+            console.log("Calendar View");
+            this.Activate_Calendar_View();
 		});
 
-		// // This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-		// 	// Called when the user clicks the icon.
-		// 	new Notice('This is a notice!');
-		// });
-		// // Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		}); 
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 1000));
 	}
 
 	onunload() {
 
-		console.log("unloded")
+		console.log('unloded')
  
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    async Activate_Calendar_View() {
+		const { workspace } = this.app;
+	
+		// let leaf = workspace.getLeaf(false);
+		
+        let leaf = workspace.getActiveViewOfType(MarkdownView)?.leaf
+
+        let file = workspace.getActiveFile()
+
+        console.log(file?.name)
+
+        // console.log(leaf.view.getViewType())
+        
+        // const VIEW_MARKDOWN = "markdown"
+        
+        // this.app.fileManager.processFrontMatter()
+
+        if (leaf)
+        {
+			await leaf.setViewState({ type: VIEW_CALENDAR_VIEW, active: true });
+            workspace.revealLeaf(leaf);
+        }
+
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+export const VIEW_CALENDAR_VIEW = "calendar-view";
+
+export class Calendar_View extends ItemView {
+	constructor(leaf: WorkspaceLeaf) {
+	    super(leaf);
 	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+  
+	getViewType() {
+	    return VIEW_CALENDAR_VIEW;
 	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+  
+	getDisplayText() {
+	    return "Calendar View";
 	}
-}
+  
+	async onOpen() {
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: HelloWorldPlugin;
+        console.log("Open View")
 
-	constructor(app: App, plugin: HelloWorldPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+        const container = this.containerEl.children[1];
+        container.empty();
+        container.createEl("h4", { text: "Calendar View" });
 	}
+  
+	async onClose() {
 
-	display(): void {
-		const {containerEl} = this;
+        console.log("Close View")
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	    // Nothing to clean up.
 	}
 }
